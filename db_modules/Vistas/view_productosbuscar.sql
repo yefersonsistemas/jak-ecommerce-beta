4ff7717a29625f7c6bd3b01e8d6f5a91d2122f38
stragie <joelenrikesoteldo@gmail.com>
2020-05-28 16:20:33 -0400

    actualizacion view_productosbuscar.sql

--- db_modules/Vistas/view_productosbuscar.sql
+++ db_modules/Vistas/view_productosbuscar.sql
@@ -2,13 +2,14 @@
 
 -- DROP VIEW public.view_productosbuscar;
 
-CREATE OR REPLACE VIEW public.view_productosbuscar AS 
- SELECT tbl_productos.name,
-    tbl_productos."imgSrc" as images,
-    tbl_productos."actualPrice" as price,
-    "tbl_poductsType".name as type
+CREATE OR REPLACE VIEW view_productosbuscar AS 
+ SELECT tbl_productos.code,
+ tbl_productos.name,
+    tbl_productos."imgSrc" AS images,
+    tbl_productos."actualPrice" AS price,
+    "tbl_poductsType".name AS type
    FROM tbl_productos
- left join "tbl_poductsType" on tbl_productos."typeCode" = "tbl_poductsType".code
+     LEFT JOIN "tbl_poductsType" ON tbl_productos."typeCode"::text = "tbl_poductsType".code::text;
 
 ALTER TABLE public.view_productosbuscar
   OWNER TO postgres;
