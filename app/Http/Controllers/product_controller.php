c109f0728bf3a015e94e173aa56ceb976f7f6ad5
cebc3541 <cbarrios3541@gmail.com>
2020-05-28 12:04:56 -0400

    controller producto

--- app/Http/Controllers/product_controller.php
+++ app/Http/Controllers/product_controller.php
@@ -3,62 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class product_controller extends Controller
 {
     //
     function index()
     {
-        $name             = 'nombre del producto';
-        $shortDescription = 'breve descripcion';
-        $description      = 'descripcion completa';
-        $images             = '[
-                                {
-                                    "src"    : "images/slider4.jpg"
-                                },
-                                {
-                                    "src"    : "images/slider2.jpg"
-                                },
-                                {
-                                    "src"    : "images/slider3.jpg"
-                                },
-                                {
-                                    "src"    : "images/slider1.jpg"
-                                }
-                            ]';
-        $price            = '50.00';
-        $discount         = '25.00';
-        $countReview      = '50';
-        $review           = '[
-                                {
-                                    "name"    : "pedro perez",
-                                    "avatar": "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-                                    "comment"    : "buenisimo",
-                                    "puntation" : 2
-                                },
-                                {
-                                    "name"    : "sandra perez",
-                                    "avatar": "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-                                    "comment"    : "bueno",
-                                    "puntation" : 1
-                                }
-                            ]';
-        $puntation        = 0;
-        $materials        = 'materiales';
-
         
-        return response()->json([
-            'name'             => $name,
-            'shortDescription' => $shortDescription,
-            'description'      => $description,
-            'price'            => $price ,
-            'images'           => json_decode($images),
-            'discount'         => $discount,
-            'countReview'      => $countReview,
-            'review'           => json_decode($review),
-            'puntation'        => $puntation,
-            'materials'        => $materials,
-        ]);
+        // $name             = 'nombre del producto';
+        // $shortDescription = 'breve descripcion';
+        // $description      = 'descripcion completa';
+        // $images             = '[
+        //                         {
+        //                             "src"    : "images/slider4.jpg"
+        //                         },
+        //                         {
+        //                             "src"    : "images/slider2.jpg"
+        //                         },
+        //                         {
+        //                             "src"    : "images/slider3.jpg"
+        //                         },
+        //                         {
+        //                             "src"    : "images/slider1.jpg"
+        //                         }
+        //                     ]';
+        // $price            = '50.00';
+        // $discount         = '25.00';
+        // $countReview      = '50';
+        // $review           = '[
+        //                         {
+        //                             "name"    : "pedro perez",
+        //                             "avatar": "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        //                             "comment"    : "buenisimo",
+        //                             "puntation" : 2
+        //                         },
+        //                         {
+        //                             "name"    : "sandra perez",
+        //                             "avatar": "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        //                             "comment"    : "bueno",
+        //                             "puntation" : 1
+        //                         }
+        //                     ]';
+        // $puntation        = 0;
+        // $materials        = 'materiales';
+
+        // $images             = 
+        $colum1 = '"productCode"';
+        $reviewpunt = DB::select("select * from  view_reviewpuntuacion where ".$colum1." = 'PRD-001'")[0];
+        $countReview      = $reviewpunt->countreview;
+        $review           = DB::select("select * from  view_review where code = 'PRD-001'");
+        $puntation        = $reviewpunt->puntation/$reviewpunt->countreview;
+        $productData = DB::select("select * from  view_productos where code = 'PRD-001'")[0];
+        return response()->json([$productData,$review,$countReview,$puntation], 200);
+        // return response()->json(, 200);
+        // return response()->json([
+        //     'name'             => $name,
+        //     'shortDescription' => $shortDescription,
+        //     'description'      => $description,
+        //     'price'            => $price ,
+        //     'images'           => json_decode($images),
+        //     'discount'         => $discount,
+        //     'countReview'      => $countReview,
+        //     'review'           => json_decode($review),
+        //     'puntation'        => $puntation,
+        //     'materials'        => $materials,
+        // ]);
     }
 
     function productSearch(){
@@ -83,7 +93,7 @@ class product_controller extends Controller
                 }
             ]';
 
-        return response()->json(json_decode($productSearch));
+            return response()->json(DB::select("select * from  view_productosbuscar"), 200);
 
     }
     function OffertProduct(){
